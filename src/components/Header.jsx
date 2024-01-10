e512dd81ca3a7dd926797127b64a67e5b2553eb5
Ricardo Quebrada <daironquebrada@gmail.com>
2024-01-10 08:27:20 -0500

    Versionando - Subiendo proyecto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
   return (
     <div className="flex justify-between pb-6">
         <div className="w-10/12 text-center">
-        <p className="font-bold">PIJAOS SALUD EPS INDIGENA</p>
-        <p className="text-sm">Oficina Puerto Gaitán</p>
-        <p className="text-[10px] text-gray-500">RES 013 DE LA DIR GENERAL DE ASUNTOS INDIGENAS DEL MIN DEL INTERIOR</p>
+        <p className="font-bold">EXAMPLE EMPRESA</p>
+        <p className="text-sm">Oficina Colombia</p>
+        <p className="text-[10px] text-gray-500">RES 013 DE LA DIR GENERAL DE MINISTERIO DEL INTERIOR</p>
       </div>
       <div className="w-5/12 flex justify-end items-center">
         <Image alt='logo' className="w-[80px] h-[80px]" src={icono} />
